37231dd41a699835f0f5e4678cdf13c2c4599d16
Deepanshu Aggarwal <aggarwaldeepanshu96@gmail.com>
2019-05-24 10:39:11 +0530

    Add relationships among users

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,21 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
 
-resources :users
+#resources :users
+
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
 resources :account_activations, only: [:edit]
 
 resources :password_resets,     only: [:new, :create, :edit, :update]
 
 resources :microposts,          only: [:create, :destroy]
+
+resources :relationships,       only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
